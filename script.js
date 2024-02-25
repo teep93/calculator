d184499203a7e2f609969dea58efe00a73ee5a59
teep93 <roz@DESKTOP-FI7C3B1>
2024-02-25 14:02:27 +0800

    add operate function

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
-
+let firstNumber = 10;
+let secondNumber = 15;
+let operator = "";
 
 function addNum(num1, num2) {
-    let sum = num1 + num2;
-    return sum
+  let sum = num1 + num2;
+  return sum;
 }
 
 function subtractNum(num1, num2) {
-    let sum = num1 - num2;
-    return sum;
+  let sum = num1 - num2;
+  return sum;
 }
 
 function multiplyNum(num1, num2) {
-    let sum = num1 * num2;
-    return sum;
+  let sum = num1 * num2;
+  return sum;
 }
 
 function divideNum(num1, num2) {
-    let sum = num1 / num2;
-    return sum;
+  let sum = num1 / num2;
+  return sum;
+}
+
+// let addition = addNum(firstNumber, secondNumber);
+// console.log(addition);
+// let subtraction = subtractNum(firstNumber, secondNumber);
+// console.log(subtraction);
+// let multiply = multiplyNum(firstNumber, secondNumber);
+// console.log(multiply);
+// let division = divideNum(firstNumber, secondNumber);
+// console.log(division);
+
+
+
+function operate(firstNumber, secondNumber, operator) {
+  if (operator === "+") {
+    addNum(firstNumber, secondNumber);
+    let addition = addNum(firstNumber, secondNumber);
+    return addition;
+  } else if (operator === "-") {
+    subtractNum(firstNumber, secondNumber);
+    let subtraction = subtractNum(firstNumber, secondNumber);
+    return subtraction;
+  } else if (operator === "*") {
+    multiplyNum(firstNumber, secondNumber);
+    let multiply = multiplyNum(firstNumber, secondNumber);
+    return multiply;
+  } else if (operator === "/") {
+    divideNum(firstNumber, secondNumber);
+    let division = divideNum(firstNumber, secondNumber);
+    return division;
+    
+  }
 }
 
 
-let addition = addNum(1, 2);
-console.log(addition);
-let subtraction = subtractNum(5, 2);
-console.log(subtraction);
-let multiply = multiplyNum(2, 2);
-console.log(multiply);
-let division = divideNum(10, 2);
-console.log(division);
+let operation = operate(firstNumber, secondNumber, "*");
+console.log(operation);
