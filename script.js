c022a06579d4dd767984f0d74b0248ba06636d7b
teep93 <roz@DESKTOP-FI7C3B1>
2024-02-25 13:22:21 +0800

    add operator functions

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+function addNum(num1, num2) {
+    let sum = num1 + num2;
+    return sum
+}
+
+function subtractNum(num1, num2) {
+    let sum = num1 - num2;
+    return sum;
+}
+
+function multiplyNum(num1, num2) {
+    let sum = num1 * num2;
+    return sum;
+}
+
+function divideNum(num1, num2) {
+    let sum = num1 / num2;
+    return sum;
+}
+
+
+let addition = addNum(1, 2);
+console.log(addition);
+let subtraction = subtractNum(5, 2);
+console.log(subtraction);
+let multiply = multiplyNum(2, 2);
+console.log(multiply);
+let division = divideNum(10, 2);
+console.log(division);
