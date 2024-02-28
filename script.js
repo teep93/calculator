30ef88910f98dd31b8c3ce31511460051eb84965
teep93 <roz@DESKTOP-FI7C3B1>
2024-02-28 23:46:02 +0800

    fix various bugs

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let firstNumber = 0;
-let secondNumber = 0;
-let operator = "";
-let displayValue1 = ''
-let displayValue2 = '';
+let firstNumber = displayNumberClick();
+let secondNumber = displayNumberClickTwo();
+let operator = displayOperatorClick();
+let displayValue1 = "";
+let displayValue2 = "";
 
 function addNum(num1, num2) {
   let sum = num1 + num2;
@@ -41,42 +41,79 @@ function operate(firstNumber, secondNumber, operator) {
     divideNum(firstNumber, secondNumber);
     let division = divideNum(firstNumber, secondNumber);
     return division;
-    
   }
 }
 
 function displayNumberClick() {
-    const displayContainer = document.querySelector('.display');
-    document.querySelectorAll('.number-button').forEach((item) => {
-        item.addEventListener('click', (e) => {
-            let content = displayContainer.textContent;
-            if (content === "/" || content === 'x' || content === "+" || content === "-") {
-                displayContainer.innerHTML = '';
-            }
-            displayContainer.innerHTML += e.target.textContent.trim();
-            
-            
-
-        })
-    })
+  const displayContainer = document.querySelector(".display");
+  document.querySelectorAll(".number-button").forEach((item) => {
+    item.addEventListener("click", (e) => {
+      let operator = displayContainer.textContent;
+      if (
+        operator === "/" ||
+        operator === "x" ||
+        operator === "+" ||
+        operator === "-"
+      ) {
+        displayContainer.innerHTML = "";
+      }
+        displayContainer.innerHTML += e.target.textContent;
+        let result = displayContainer.textContent;
+        Number(result);
+        console.log(result);
+        return result;
+        
+    });
+  });
 }
+
+function displayNumberClickTwo() {
+    const displayContainer = document.querySelector(".display");
+    document.querySelectorAll(".number-button").forEach((item) => {
+      item.addEventListener("click", (e) => {
+        let operator = displayContainer.textContent;
+        if (
+          operator === "/" ||
+          operator === "x" ||
+          operator === "+" ||
+          operator === "-"
+        ) {
+          displayContainer.innerHTML = "";
+        }
+          displayContainer.innerHTML += e.target.textContent;
+          let result = displayContainer.textContent;
+          Number(result);
+          console.log(result);
+          return result;
+          
+      });
+    });
+  }
 
 function displayOperatorClick() {
-    const displayContainer = document.querySelector('.display');
-    document.querySelectorAll('.operator-button').forEach((item) => {
-        item.addEventListener('click', (e) => {
-            displayContainer.innerHTML = '';
-            displayContainer.innerHTML = e.target.textContent.trim();
+  const displayContainer = document.querySelector(".display");
+  document.querySelectorAll(".operator-button").forEach((item) => {
+    item.addEventListener("click", (e) => {
+    displayContainer.innerHTML = "";
+    displayContainer.innerHTML = e.target.textContent.trim();
+    let result = displayContainer.textContent;
+    console.log(result);
+    return result;    
+        
+    });
+  });
+}
 
-        })
-    })
+function displayOperate() {
+    
 }
 
 
-displayNumberClick();
-displayOperatorClick();
 
-let operation = operate(firstNumber, secondNumber, "*");
+let operation = operate(firstNumber, secondNumber, operator);
 console.log(operation);
 
-
+let hello = document.querySelector(".hello")
+console.log(hello)
+let hello2 = document.querySelector(".hello2");
+console.log(hello2);
